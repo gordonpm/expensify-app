07eba664c94fe8f2cf08c04633027a9d8674054c
gordonpm <gordonm0815@gmail.com>
2020-04-01 09:51:11 -0700

    cleanup unwanted files

--- src/person.js
+++
@@ -1,9 +0,0 @@
-const isAdult = (age) => age >= 18;
-const canDrink = (age) => age >= 21;
-
-//export default (age) => age >= 65;
-
-const isSenior = (age) => age >= 65;
-//export default (age) => age >= 65;
-
-export { isAdult, canDrink, isSenior as default };
