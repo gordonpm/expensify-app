dc373eb4a8c8ae07f8f3cea634c0bbcd410cddd5
gordonpm <gordonm0815@gmail.com>
2020-04-15 14:48:37 -0700

    1) Added edit and remove expense 2) Added data range picker on dashboard page 3) Added dispatches to Dashboard page after editing or removing an expense

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
+import { editExpense, removeExpense } from '../actions/expenses';
+
 
 const EditExpensePage = (props) => {
-    console.log(props);
     return (
         <div>
             <ExpenseForm 
+                expense={props.expense}
                 onSubmit={(expense) => {
-                    console.log('updated', expense);
+                    props.dispatch(editExpense(props.expense.id, expense));
+                    props.history.push('/'); // goto Dashboard page
                 }}
             />
+            <button onClick={() => {
+                props.dispatch(removeExpense({ id: props.expense.id }));
+                props.history.push('/'); // goto Dashboard page
+            }}>Remove</button>
         </div>
-    );
+    );  
 };
 
 const mapStateToProps = (state, props) => {
--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -7,14 +7,17 @@ const now = moment();
 console.log(now.format('MMM Do, YYYY'));
 
 export default class ExpenseForm extends React.Component {
-    state = {
-        description: '',
-        note: '',
-        amount: '',
-        createdAt: moment(),
-        calendarFocused: false,
-        error: ''
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            description: props.expense ? props.expense.description : '',
+            note: props.expense ? props.expense.note : '',
+            amount: props.expense ? (props.expense.amount / 100).toString() : '',
+            createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
+            calendarFocused: false,
+            error: ''
+        };
+    }
     onDescriptionChange = (e) => {
         const description = e.target.value;
         this.setState(() => ({ description }));
@@ -40,9 +43,7 @@ export default class ExpenseForm extends React.Component {
     onSubmit = (e) => {
         e.preventDefault();
         if (!this.state.description || !this.state.amount) {
-            // set error
-            console.log('error');
-            this.setState(() => ({ error: 'Please enter description and amount' }));
+            this.setState(() => ({ error: 'Please enter description and amount' }));   // set error
         } else {
             this.setState(() => ({ error: '' }));
             this.props.onSubmit({
