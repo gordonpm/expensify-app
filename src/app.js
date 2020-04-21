a86d9c99d8ff2c5b644dad6694a9b216a0d870ba
gordonpm <gordonm0815@gmail.com>
2020-04-21 10:37:36 -0700

    1) Added enzyme tests for ExpenseForm 2) Added mock for moment

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css'; // reset for all browsers
 import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
